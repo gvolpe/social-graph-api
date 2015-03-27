36aacfed323cd00a602d1e4f13be1c1f1db87bbf
Gabi Volpe <gvolpe@dridco.com>
2015-03-27 14:59:14 -0300

    Dependency injection refactor.

--- app/controllers/RelationshipController.scala
+++ app/controllers/RelationshipController.scala
@@ -4,40 +4,36 @@ import auth.module.{AuthenticatorIdentityModule, JWTAuthenticatorController, Red
 import model._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json._
-import repository.{NeoRelationshipRepositoryProvider, RelationshipRepository}
+import repository.{NeoRelationshipRepository, RelationshipRepository}
 
 object RelationshipController extends BaseRelationshipController
-  with RedisAuthenticatorIdentityModule with NeoRelationshipRepositoryProvider
+  with RedisAuthenticatorIdentityModule with NeoRelationshipRepository
 
 trait BaseRelationshipController extends JWTAuthenticatorController {
 
-  self: AuthenticatorIdentityModule =>
-
-  def repo: RelationshipRepository
+  self: AuthenticatorIdentityModule with RelationshipRepository =>
 
   def findFollowers(id: Long) = SecuredAction.async { implicit request =>
-    val followers = repo.find(Follower, id)
-    followers.map(ids => Ok(Json.toJson(ids)))
+    find(Follower, id) map (ids => Ok(Json.toJson(ids)))
   }
 
   def findFriends(id: Long) = SecuredAction.async { implicit request =>
-    val friends = repo.find(Friend, id)
-    friends.map(ids => Ok(Json.toJson(ids)))
+    find(Friend, id) map (ids => Ok(Json.toJson(ids)))
   }
 
   def createFriendship = SecuredAction.async(parse.json[Friendship]) { implicit request =>
     val friendship: Friendship = request.body
     for {
-      friend <- repo.create(Friend, friendship.me, friendship.friend)
-      follower <- repo.create(Follower, friendship.friend, friendship.me)
+      friend <- create(Friend, friendship.me, friendship.friend)
+      follower <- create(Follower, friendship.friend, friendship.me)
     } yield (Ok)
   }
 
   def deleteFriendship = SecuredAction.async(parse.json[Friendship]) { implicit request =>
     val friendship: Friendship = request.body
     for {
-      friend <- repo.delete(Friend, friendship.me, friendship.friend)
-      follower <- repo.delete(Follower, friendship.friend, friendship.me)
+      friend <- delete(Friend, friendship.me, friendship.friend)
+      follower <- delete(Follower, friendship.friend, friendship.me)
     } yield (Ok)
   }
 
--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -5,35 +5,33 @@ import model.User._
 import model._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json._
-import repository.{NeoUserRepositoryProvider, UserRepository}
+import repository.{NeoUserRepository, UserRepository}
 
 object UserController extends BaseUserController
-  with RedisAuthenticatorIdentityModule with NeoUserRepositoryProvider
+  with RedisAuthenticatorIdentityModule with NeoUserRepository
 
 trait BaseUserController extends JWTAuthenticatorController {
 
-  self: AuthenticatorIdentityModule =>
-
-  def repo: UserRepository
+  self: AuthenticatorIdentityModule with UserRepository =>
 
   def findUserById(id: Long) = SecuredAction.async { implicit request =>
-    repo.findById(id) map {
+    findById(id) map {
       case Some(user) => Ok(Json.toJson(user))
       case None => NotFound
     }
   }
 
   def findUsers = SecuredAction.async { implicit request =>
-    repo.findAll map (user => Ok(Json.toJson(user)))
+    findAll map (user => Ok(Json.toJson(user)))
   }
 
   def createUser = SecuredAction.async(parse.json[UserCreation]) { implicit request =>
     val user: UserCreation = request.body
-    repo.create(user) map (id => Created(Json.toJson(id)))
+    create(user) map (id => Created(Json.toJson(id)))
   }
 
   def deleteUser(id: Long) = SecuredAction.async { implicit request =>
-    repo.delete(id) map (_ => Ok)
+    delete(id) map (_ => Ok)
   }
 
 }
